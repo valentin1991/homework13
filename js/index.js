c581a9306e414fb3612335ed7f3e73dee8bf6cd1
valentin1991 <valentin.semenov.1991@mail.ru>
2020-03-09 19:48:20 +0300

    homework13

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 // 1) Создайте переменную со значением 5, а также создайте программу, которая будет просить пользователя ввести число из всплывающего окна и сверять это число с числом в первой переменной. До тех пор, пока пользователь не введет число 5, просите пользователя это сделать и выдавайте ему всплывающее окно.
 let v = 5;
-;
+while (prompt("Введите число",5) != v) {
+  prompt("Введите число",5);
+}
+// 2) Напишите программу на JavaScript, которая будет получать ширину и высоту окна (при чем каждый раз, когда размер окна изменяется).
+window.onresize = function(){
 
-while (prompt("Введите число",5) != 5) {
-  prompt("Введите число",5);      
+var w = window.innerWidth
+  document.documentElement.clientWidth
+  document.body.clientWidth;
+
+var h = window.innerHeight
+  document.documentElement.clientHeight
+  document.body.clientHeight;
+
+var x = document.getElementById("demo");
+x.innerHTML = "Ширина окна: " + w + ", Высота: " + h + ".";
 }
